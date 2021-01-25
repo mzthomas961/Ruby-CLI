762a3a4f4ba56a08738f727884021b430b56feae
mzthomas961 <74322580+mzthomas961@users.noreply.github.com>
2021-01-25 13:29:27 -0500

    Add files via upload

---
+++ Ruby-CLI-Setup/app/yourAppName.rb
@@ -0,0 +1,16 @@
+class YourAppName
+  # here will be your CLI!
+  # it is not an AR class so you need to add attr
+
+  def run
+    # welcome
+    # login_or_signup
+    # wanna_see_favs?
+    # some_method(some_argument)
+    # exit
+  end
+
+  private
+
+  
+end
